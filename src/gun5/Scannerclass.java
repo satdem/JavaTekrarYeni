ee95765c80c45f711c8428ac4061f34bc390d5f4
satı demirci <69872891+satdem@users.noreply.github.com>
2020-09-05 21:26:21 +0200

    javatekrar ekleme

---
+++ src/gun5/Scannerclass.java
@@ -0,0 +1,12 @@
+package gun5;
+
+import java.util.Scanner;
+
+public class Scannerclass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("biletiniz çift yönlü olsun mu?");
+        
+    }
+}
